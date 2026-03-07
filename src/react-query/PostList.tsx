10a0c74152db0c24c837a5c400d5706f924db0a0
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-07 13:42:32 +0300

    Parameterized queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,12 +1,15 @@
 // import axios from 'axios';
 // import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import usePosts from './hooks/usePosts';
 
 const PostList = () => {
   // const [posts, setPosts] = useState<Post[]>([]);
   // const [error, setError] = useState('');
 
-  const { data, error, isLoading } = usePosts();
+  const [userId, setUserId] = useState<number>();
+
+  const { data, error, isLoading } = usePosts(userId);
 
   // useEffect(() => {
   //   axios
@@ -20,6 +23,14 @@ const PostList = () => {
   if (error) return <p>{error.message}</p>;
 
   return (
+    <>
+    <select className="form-select mb-3" 
+      onChange={(event) => setUserId(parseInt(event.target.value))} value={userId}>
+      <option value=""></option>
+      <option value="1">User 1</option>
+      <option value="2">User 2</option>
+      <option value="3">User 3</option>
+    </select>
     <ul className="list-group">
       {data?.map((post) => (
         <li key={post.id} className="list-group-item">
@@ -27,6 +38,7 @@ const PostList = () => {
         </li>
       ))}
     </ul>
+    </>
   );
 };
 
