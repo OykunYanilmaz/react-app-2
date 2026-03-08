a3851c4851cd82fbe7ac753594420bbf3b4fc606
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-08 16:33:47 +0300

    Consolidating state logic with a reducer and creating complex actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import './App.css';
 import PostList from './react-query/PostList';
 import TodoForm from './react-query/TodoForm';
 import TodoList from './react-query/TodoList';
+import Counter from './state-management/Counter';
+import TaskList from './state-management/TaskList';
 
 function App() {
   // return <TodoList />;
   return (
-    <>
-      <TodoForm />
-      <TodoList />
-    </>
+    // <>
+    //   <TodoForm />
+    //   <TodoList />
+    // </>
+    <TaskList />
   );
 }
 
